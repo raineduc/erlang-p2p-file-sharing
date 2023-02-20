0b68aeeb05be602f440d43bde5e258ea37a7ce34
олег <oleg.ru1234567890@mail.ru>
2023-02-20 14:23:25 +0300

    update torrent reader

--- src/ep2psharing_read_torrent_file.erl
+++ src/ep2psharing_read_torrent_file.erl
@@ -1,7 +1,7 @@
 -module(ep2psharing_read_torrent_file).
 
 %% API
--export([read_torrent_file/1]).
+-export([read_torrent_file/1, start/0]).
 -include("ep2psharing_metainfo.hrl").
 
 read_torrent_file(FilePath) ->
@@ -9,13 +9,13 @@ read_torrent_file(FilePath) ->
     {ok, BinaryData} ->
       case bencode:decode(BinaryData) of
         {ok, DecodedData} ->
-          case maps:find("info", DecodedData) of
+          case maps:find(<<"info">>, DecodedData) of
             {ok, InfoData} ->
               #metainfo_info_field{
-                name = maps:get("name", InfoData, ""),
-                piece_len = maps:get("piece length", InfoData, 0),
-                pieces = parse_pieces_data(maps:get("pieces", InfoData, <<>>)),
-                length = maps:get("length", InfoData, 0)
+                name = maps:get(<<"name">>, InfoData, ""),
+                piece_len = maps:get(<<"piece length">>, InfoData, 0),
+                pieces = parse_pieces_data(maps:get(<<"pieces">>, InfoData, <<>>)),
+                length = maps:get(<<"length">>, InfoData, 0)
               };
             error ->
               io:format("Error: 'info' key not found in torrent file.~n", []),
@@ -34,3 +34,13 @@ parse_pieces_data(<<>>) -> [];
 parse_pieces_data(Data) ->
   <<Hash:20/binary, Rest/binary>> = Data,
   [Hash | parse_pieces_data(Rest)].
+
+start() ->
+  % read torrent file
+  Metainfo = read_torrent_file("test.torrent"),
+  % print info
+  io:format("Torrent file info:~n", []),
+  io:format("Name: ~p~n", [Metainfo#metainfo_info_field.name]),
+  io:format("Piece length: ~p~n", [Metainfo#metainfo_info_field.piece_len]),
+  io:format("Pieces: ~p~n", [Metainfo#metainfo_info_field.pieces]),
+  io:format("Length: ~p~n", [Metainfo#metainfo_info_field.length]).
