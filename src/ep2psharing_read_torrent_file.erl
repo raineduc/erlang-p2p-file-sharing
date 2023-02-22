aacb1eb8a96b5a7fd3de87119ed0bcca1e30cd09
олег <oleg.ru1234567890@mail.ru>
2023-02-22 09:10:26 +0300

    lint + format

--- src/ep2psharing_read_torrent_file.erl
+++ src/ep2psharing_read_torrent_file.erl
@@ -2,44 +2,49 @@
 
 %% API
 -export([read_torrent_file/1]).
+
 -include("ep2psharing_metainfo.hrl").
 
 read_torrent_file(FilePath) ->
-  case file:read_file(FilePath) of
-    {ok, BinaryData} ->
-      case bencode:decode(BinaryData) of
-        {ok, DecodedData} ->
-          Announce = parse_announce(maps:get(<<"announce">>, DecodedData)),
-          case maps:find(<<"info">>, DecodedData) of
-            {ok, InfoData} ->
-              InfoField = #metainfo_info_field{
-                name = binary:bin_to_list(maps:get(<<"name">>, InfoData, "")),
-                piece_len = maps:get(<<"piece length">>, InfoData, 0),
-                pieces = parse_pieces_data(maps:get(<<"pieces">>, InfoData, <<>>)),
-                length = maps:get(<<"length">>, InfoData, 0)
-              },
-              InfoHash = ep2psharing_bencoding:calc_info_field_hash(InfoField),
-              #metainfo{ announce = Announce, info = InfoField, info_hash = InfoHash };
-            error ->
-              io:format("Error: 'info' key not found in torrent file.~n", []),
-              undefined
-          end;
-        error ->
-          io:format("Error: failed to decode bencoded data.~n", []),
-          undefined
-      end;
-    {error, Reason} ->
-      io:format("Error: failed to read file: ~p~n", [Reason]),
-      undefined
-  end.
+    case file:read_file(FilePath) of
+        {ok, BinaryData} ->
+            case bencode:decode(BinaryData) of
+                {ok, DecodedData} ->
+                    Announce = parse_announce(maps:get(<<"announce">>, DecodedData)),
+                    case maps:find(<<"info">>, DecodedData) of
+                        {ok, InfoData} ->
+                            InfoField =
+                                #metainfo_info_field{name =
+                                                         binary:bin_to_list(
+                                                             maps:get(<<"name">>, InfoData, "")),
+                                                     piece_len =
+                                                         maps:get(<<"piece length">>, InfoData, 0),
+                                                     pieces =
+                                                         parse_pieces_data(maps:get(<<"pieces">>,
+                                                                                    InfoData,
+                                                                                    <<>>)),
+                                                     length = maps:get(<<"length">>, InfoData, 0)},
+                            InfoHash = ep2psharing_bencoding:calc_info_field_hash(InfoField),
+                            #metainfo{announce = Announce,
+                                      info = InfoField,
+                                      info_hash = InfoHash};
+                        error ->
+                            erlang:error("Error: 'info' key not found in torrent file.~n", [])
+                    end;
+                error ->
+                    erlang:error("Error: failed to decode bencoded data.~n", [])
+            end;
+        {error, Reason} ->
+            erlang:error("Error: failed to read file: ~p~n", [Reason])
+    end.
 
-parse_pieces_data(<<>>) -> [];
-
+parse_pieces_data(<<>>) ->
+    [];
 parse_pieces_data(Data) ->
-  <<Hash:20/binary, Rest/binary>> = Data,
-  [Hash | parse_pieces_data(Rest)].
+    <<Hash:20/binary, Rest/binary>> = Data,
+    [Hash | parse_pieces_data(Rest)].
 
 parse_announce(BitString) ->
-  Str = binary_to_list(BitString),
-  [Pid, Node] = string:split(Str, ","),
-  {list_to_atom(Pid), list_to_atom(Node)}.
+    Str = binary_to_list(BitString),
+    [Pid, Node] = string:split(Str, ","),
+    {list_to_atom(Pid), list_to_atom(Node)}.
