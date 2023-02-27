279845dd54ffb725b3ed3820ab56a7ab50e05e25
олег <oleg.ru1234567890@mail.ru>
2023-02-27 09:45:32 +0300

    add console for client

--- src/ep2psharing_client.erl
+++ src/ep2psharing_client.erl
@@ -2,8 +2,8 @@
 
 -behaviour(gen_server).
 
--export([start_link/0, download/1]).
--export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2]).
+-export([start_link/0, download/1, start_console/0]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2]).
 
 -record(state, {torrents = dict:new()}).
 
@@ -21,13 +21,13 @@ handle_call({download, TorrentInfoHash}, _From, State) ->
     {error, _} ->
       {reply, {error, "Torrent not found"}, State};
     {ok, {Torrent, TorrentPid}} ->
-      case ep2psharing:download_status(TorrentPid) of
+      case ep2psharing_torrent:download_status(TorrentPid) of
         {downloaded, FilePath} ->
           {reply, {ok, FilePath}, State};
         {downloading, _} ->
           {reply, {error, "Torrent is already being downloaded"}, State};
         {not_started, _} ->
-          _TorrentPid = ep2psharing:start(Torrent),
+          _TorrentPid = ep2psharing_torrent:start(Torrent),
           NewState = State#state{torrents = dict:store(TorrentInfoHash, {Torrent, _TorrentPid}, State#state.torrents)},
           {reply, ok, NewState}
       end
@@ -39,5 +39,30 @@ handle_cast(_Msg, State) ->
 handle_info(_Info, State) ->
   {noreply, State}.
 
-terminate(_Reason, _State) ->
-  ok.
+start_console() ->
+  io:format("Ep2psharing Client console~n"),
+  io:format("Commands:~n"),
+  io:format("    download TorrentInfoHash - Download a torrent~n"),
+  io:format("    exit - Exit console~n"),
+  loop().
+
+loop() ->
+  case io:get_line("> ") of
+    {error, _} ->
+      exit(normal);
+    Input ->
+      case string:tokens(Input, " \t\n\r") of
+        ["download", TorrentInfoHash] ->
+          case download(TorrentInfoHash) of
+            {ok, FilePath} ->
+              io:format("Torrent downloaded to: ~s~n", [FilePath]);
+            {error, Error} ->
+              io:format("Error: ~s~n", [Error])
+          end;
+        ["exit"] ->
+          exit(normal);
+        _ ->
+          io:format("Invalid command~n")
+      end,
+      loop()
+  end.
