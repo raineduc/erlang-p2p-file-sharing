1f580bbb6c593cf54ae5b50a4d2fe1b371b5f2e3
олег <oleg.ru1234567890@mail.ru>
2023-03-02 18:41:36 +0300

    add simple test

--- src/test_ep2psharing.erl
+++ src/test_ep2psharing.erl
@@ -3,34 +3,30 @@
 -include_lib("eunit/include/eunit.hrl").
 
 start_peer_and_send_download_request() ->
-  net_kernel:start(['tracker@DESKTOP-D09RERH', shortnames]),
+  % Создаем новые ноды
+  net_kernel:start([tracker, shortnames]),
+  net_kernel:start([node, shortnames]),
   net_kernel:start([node1, shortnames]),
-  net_kernel:start([node2, shortnames]),
-  spawn('tracker@DESKTOP-D09RERH', fun() ->
-    ep2psharing_tracker_sup:start_link()
-                                        end),
-  % Создаем новый процесс Erlang
-  % Считываем информацию о торрент-файле
-  TorrentFile = ep2psharing_read_torrent_file:read_torrent_file("path/big.txt.torrent"),
-  % Формируем запрос на скачивание файла
-  DownloadRequest = {download_request, TorrentFile,  "path/big.txt"},
-  DownloadRequestDownload = {download_request, TorrentFile,  "path/bigtest.txt"},
 
-  spawn(peer@node1, fun() ->
+  % Считываем информацию о торрент-файле
+  TorrentFile = ep2psharing_read_torrent_file:read_torrent_file("./example/big.txt.torrent"),
+  % Формируем запрос на скачивание файла
+  DownloadRequest = {download_request, TorrentFile,  "./example/big.txt"},
+  DownloadRequestDownload = {download_request, TorrentFile,  "./example/bigtest.txt"},
+  Pid = self(),
+  spawn(tracker, fun() ->
+    ep2psharing_tracker_sup:start_link()
+                 end),
+
+  spawn(node, fun() ->
     ep2psharing_peer_sup:start_link(),
-    peer ! {torrent, self(), DownloadRequest}
+    peer ! {torrent, Pid, DownloadRequest}
                       end),
-  spawn(peer@node2, fun() ->
+  spawn(node1, fun() ->
     ep2psharing_peer_sup:start_link(),
-    peer ! {torrent, self(), DownloadRequestDownload}
-                      end),
-  receive
-    {torrent, _Pid, DownloadRequest} -> ok
-  after 30000 -> error
-  end.
+    peer ! {torrent, Pid, DownloadRequestDownload}
+                      end).
 
 % Тест
 simple_test() ->
-  start_peer_and_send_download_request(),
-  % Проверяем, что peer_sup принял запрос на скачивание файла
-  ?assertEqual(ok, ok).
+  start_peer_and_send_download_request().
